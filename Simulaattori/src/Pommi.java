cb570dbbd191028cb68bca86bf45c0cee7e5567b
hesba <38681935+hesba@users.noreply.github.com>
2018-05-03 17:10:19 +0300

    Koulujälkeiset lisäilyt 3.5
    Ei mitää isoo tehty.

--- Simulaattori/src/Pommi.java
+++ Simulaattori/src/Pommi.java
@@ -20,11 +20,14 @@ public class Pommi {
         matka.getkulunutAika();
         sat.satunnainen();
         työpaikka.lisääTyökalu();*/
+        System.out.println("Olet pomminpurkaja. Pommin räjähtämiseen aikaa: " + Aika.aika + " minuuttia.");
+        System.out.println("Tehtävänäsi on valita mukaasi kolme (3) työkalua, jotka vaikuttavat purkuaikaan.");
+        System.out.println("Saatat kohdata matkalla satunnaisia tapahtumia, jotka vievät aikaa.");
+        System.out.println("ONNEA MATKAAN!");
         työpaikka.lisääTyökalu();
-        kuori();
-        irrotus();
-        johto();
-        
+        System.out.println("Työkalut repussa ja saavuit pommipaikalle.");
+        matka.getkulunutAika();
+        sat.millonSatunnainen();
     }
     
     
@@ -34,9 +37,13 @@ public class Pommi {
         System.out.println("Kuoren saa auki helpoiten työkalulla: " + omaTyökalu);
         if (työpaikka.omatTyökalut().contains(omaTyökalu.toString())) {
             System.out.println("Sinulta löytyi oikeat työkalu: " + omaTyökalu);
+            Aika.vähennä(1);
+            System.out.println(omaTyökalu+ " oli helkuti hyvä. Aikaa meni vain minuutti!");
         }
         else {
-            System.out.println("Sul ei oo oikeet työkaluu! rip");
+            System.out.println("Sul ei oo oikeet työkaluu! PERKELE!");
+            Aika.vähennä(5);
+            System.out.println("Siul meni sörkkies 5 minuuttii.");
         }
         System.out.println("Stressitaso " + st.getTaso() + " . Aikaa jäljellä " + Aika.aika + " minuuttia.");
     }
@@ -51,13 +58,13 @@ public class Pommi {
         else {
             System.out.println("Sul ei oo oikeet työkaluu! rip");
         }
-        System.out.println("Stressitaso " + st.getTaso() + " . Aikaa jäljellä" + Aika.aika);
+        System.out.println("Stressitaso " + st.getTaso() + " . Aikaa jäljellä " + Aika.aika +" minuuttia.");
     }
 
     public void johto() {
          System.out.println("Tehtävänä katkaista johto irti kellosta. Tää onnistuu kynsillä.");
-        System.out.println("Stressitaso " + st.getTaso() + " . Aikaa jäljellä" + Aika.aika);
-        System.out.println("Katkaise johdosita joko 1. Punainen, 2. Sininen tai 3. Musta ");
+        System.out.println("Stressitaso " + st.getTaso() + " . Aikaa jäljellä " + Aika.aika + " minuuttia.");
+        System.out.println("Katkaise johdoista joko 1. Punainen, 2. Sininen tai 3. Musta ");
         int johto = lukija.nextInt();
         if (johto!=2) {
             meltDown();
@@ -68,14 +75,18 @@ public class Pommi {
     }
 
     public void räjähde() {
-         System.out.println("Deaktivointi onnistuu helpoiten " + työkalu + " :lla");
-        if (true) {
-            System.out.println("Hyvä, oikeetyökalu mukana. Nyt varovasti!");
-            System.out.println("Stressitaso " + stressitaso + " . Aikaa jäljellä" + aika);
+        Työkalut omaTyökalu = työpaikka.arpooTyökalun();
+        System.out.println("Irrotus onnistuu helpoiten " + omaTyökalu );
+        if (työpaikka.omatTyökalut().contains(omaTyökalu.toString())) {
+            System.out.println("Sinulta löytyi oikeat työkalu: " + omaTyökalu);
+        }
+        else {
+            System.out.println("Sul ei oo oikeet työkaluu! rip");
+        }
     }
 
     public void meltDown() {
-        System.out.println("BOOOM! Pommi räjähti!");
+        Aika.vähennä(50);
     }
 
 }
--- Simulaattori/src/Satunnaiset.java
+++ Simulaattori/src/Satunnaiset.java
@@ -10,12 +10,19 @@ public class Satunnaiset {
         this.st = st;
     }
     
+    public void millonSatunnainen() {
+        int luku = random.nextInt(30)+1;
+        if (luku>20) {
+            satunnainen();
+        }
+    }
+    
     public void satunnainen() {
         int randomAika = random.nextInt(4) + 1;
         int randomStressi = random.nextInt(4) + 1;
         int valinta = random.nextInt(3) + 1;
         if (valinta == 1) {
-            System.out.println("Kierrät ja yrität ottaa Alepa-fillarin, pyöriä EI OLE, jatkat kävelyä.");
+            System.out.println("Janne tuli kysyy taas tyhmii..");
             st.lisääStressiä(randomStressi);
         }
 
@@ -27,5 +34,6 @@ public class Satunnaiset {
             System.out.println("Aikaa meni " + randomAika + " minuuttia.");
             Aika.vähennä(randomAika);
         }
+        System.out.println("Stressitaso " + st.getTaso() + ". Pommissa aikaa jäljellä: " + Aika.aika + " minuuttia.");
     }
 }
