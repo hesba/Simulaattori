3cfc5755377fb1290ab56a1a3f4761df8adfccc9
hesba <38681935+hesba@users.noreply.github.com>
2018-05-04 14:02:54 +0300

    Erkalle kopio

--- Simulaattori/src/Aika.java
+++ Simulaattori/src/Aika.java
@@ -1,10 +1,11 @@
 
 public class Aika {
 
-    public static int aika=30;
+    public static int aika = 30;
 
     public static boolean vähennä(int vähennä) {
-        if (aika - vähennä<=0) {
+        if (aika - vähennä <= 0) {
+            aika = aika - vähennä;
             System.out.println("KABOOOOM!");
             return false;
         } else {
--- Simulaattori/src/Pommi.java
+++ Simulaattori/src/Pommi.java
@@ -1,5 +1,8 @@
+
 import java.util.*;
+
 public class Pommi {
+
     static Scanner lukija = new Scanner(System.in);
     private StressiTaso st;
     private Matka matka;
@@ -20,6 +23,7 @@ public class Pommi {
         matka.getkulunutAika();
         sat.satunnainen();
         työpaikka.lisääTyökalu();*/
+
         System.out.println("Olet pomminpurkaja. Pommin räjähtämiseen aikaa: " + Aika.aika + " minuuttia.");
         System.out.println("Tehtävänäsi on valita mukaasi kolme (3) työkalua, jotka vaikuttavat purkuaikaan.");
         System.out.println("Saatat kohdata matkalla satunnaisia tapahtumia, jotka vievät aikaa.");
@@ -27,10 +31,19 @@ public class Pommi {
         työpaikka.lisääTyökalu();
         System.out.println("Työkalut repussa ja saavuit pommipaikalle.");
         matka.getkulunutAika();
-        sat.millonSatunnainen();
+        while (true) {
+            kuori();
+            sat.millonSatunnainen();
+            johto();
+            if (Aika.aika<=0) {
+                System.out.println("iffi sisäl");
+                break;
+            }
+            sat.millonSatunnainen();
+            räjähde();
+        }
     }
-    
-    
+
     public void kuori() {
         System.out.println("Tehtävänä nostaa pommi pois kuoresta.");
         Työkalut omaTyökalu = työpaikka.arpooTyökalun();
@@ -38,9 +51,8 @@ public class Pommi {
         if (työpaikka.omatTyökalut().contains(omaTyökalu.toString())) {
             System.out.println("Sinulta löytyi oikeat työkalu: " + omaTyökalu);
             Aika.vähennä(1);
-            System.out.println(omaTyökalu+ " oli helkuti hyvä. Aikaa meni vain minuutti!");
-        }
-        else {
+            System.out.println(omaTyökalu + " oli helkuti hyvä. Aikaa meni vain minuutti!");
+        } else {
             System.out.println("Sul ei oo oikeet työkaluu! PERKELE!");
             Aika.vähennä(5);
             System.out.println("Siul meni sörkkies 5 minuuttii.");
@@ -49,44 +61,36 @@ public class Pommi {
     }
 
     public void irrotus() {
-         System.out.println("Tehtävänä irrottaa pommi muista osista.");
-         Työkalut omaTyökalu = työpaikka.arpooTyökalun();
-        System.out.println("Irrotus onnistuu helpoiten " + omaTyökalu );
+        System.out.println("Tehtävänä irrottaa pommi muista osista.");
+        Työkalut omaTyökalu = työpaikka.arpooTyökalun();
+        System.out.println("Irrotus onnistuu helpoiten " + omaTyökalu);
         if (työpaikka.omatTyökalut().contains(omaTyökalu.toString())) {
             System.out.println("Sinulta löytyi oikeat työkalu: " + omaTyökalu);
-        }
-        else {
+        } else {
             System.out.println("Sul ei oo oikeet työkaluu! rip");
         }
-        System.out.println("Stressitaso " + st.getTaso() + " . Aikaa jäljellä " + Aika.aika +" minuuttia.");
+        System.out.println("Stressitaso " + st.getTaso() + " . Aikaa jäljellä " + Aika.aika + " minuuttia.");
     }
 
     public void johto() {
-         System.out.println("Tehtävänä katkaista johto irti kellosta. Tää onnistuu kynsillä.");
+        System.out.println("Tehtävänä katkaista johto irti kellosta. Tää onnistuu kynsillä.");
         System.out.println("Stressitaso " + st.getTaso() + " . Aikaa jäljellä " + Aika.aika + " minuuttia.");
         System.out.println("Katkaise johdoista joko 1. Punainen, 2. Sininen tai 3. Musta ");
         int johto = lukija.nextInt();
-        if (johto!=2) {
-            meltDown();
-        }
-        else {
+        if (johto != 2) {
+            Aika.vähennä(30);
+        } else {
             System.out.println("Jei! Oikee johto.");
         }
     }
 
     public void räjähde() {
         Työkalut omaTyökalu = työpaikka.arpooTyökalun();
-        System.out.println("Irrotus onnistuu helpoiten " + omaTyökalu );
+        System.out.println("Irrotus onnistuu helpoiten " + omaTyökalu);
         if (työpaikka.omatTyökalut().contains(omaTyökalu.toString())) {
             System.out.println("Sinulta löytyi oikeat työkalu: " + omaTyökalu);
-        }
-        else {
+        } else {
             System.out.println("Sul ei oo oikeet työkaluu! rip");
         }
     }
-
-    public void meltDown() {
-        Aika.vähennä(50);
-    }
-
 }
